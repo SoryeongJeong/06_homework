02d76ff3d964509e34d204b2a85dd9991e5c035f
SoryeongJeong <55128033+SoryeongJeong@users.noreply.github.com>
2019-10-29 08:52:44 +0900

    op_4

--- main.c
+++ main.c
@@ -3,41 +3,16 @@
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
-int sumTwo(int a, int b)
-
+void square(int a)
 {
-	int output;
-	output = a + b;
-	return output;
+	a = a*a;
 }
 
-int square(int n)
+int main()
 
 {
-	return (n*n);
-}
-
-int get_max(int a, int b)
-{
-	if (a > b)
-		return (a);
-	else
-		return (b);
-}
-
-int main (void)
-{
-	int a;
-	int b;
-	
-	a = 2;
-	b = 5;
-	int add;
-	add = sumTwo(a,b);
-	
-	printf("a+b = %d\n", add);
-	printf("a*a = %d\n", square(a));
-	printf("max = %d", get_max(a,b));
-	
+	int a = 2;
+	square(a);
+	printf("a = %i\n", a);
 	return 0;
 }
